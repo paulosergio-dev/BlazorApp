b49cd80b80bc41f2bad491776933f6fbe1f368e9
paulosergio.dev <paulosergioo-@hotmail.com>
2024-10-22 15:04:10 -0300

    Inicio dos estudos em Blazor
    Criação das categorias para testes.

--- BlazorApp/Models/Categorias.cs
+++ BlazorApp/Models/Categorias.cs
@@ -1,9 +1,13 @@
-﻿namespace BlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
 {
     public class Categorias
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "O título é obrigatório.")]
+        [RegularExpression(@"^[a-zA-ZÀ-ÿ\s]*$", ErrorMessage = "O título deve conter apenas letras.")]
         public string Titulo { get; set; } = string.Empty;
 
         public string Descricao { get; set; } = string.Empty;
